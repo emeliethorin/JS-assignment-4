abd7a9c30e827b018952cd0719e37f6257fca915
emeliethorin <emeliethorin10@gmail.com>
2025-04-08 13:39:29 +0200

    conditions set

---
+++ main.js
@@ -0,0 +1,14 @@
+//Number guessing game
+
+const target = Math.floor(Math.random() * 10) + 1;
+let guess = null;
+
+while (guess !== target) {
+    guess = parseFloat(prompt("Guess the number between 1-10"));
+    console.log(`Target: ${target} Guess: ${guess}`);
+    if (guess > target) {
+        console.log("Too high")
+    } else if (guess < target) {
+        console.log("Too low")
+    }
+}
